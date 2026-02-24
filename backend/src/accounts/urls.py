6ee59ea273268acbe986e1cdfc737b63dfe5df13
BarBaRoSa1337 <abderahmanechaqour@gmail.com>
2026-02-24 13:53:15 +0000

    Add new code and files

---
+++ backend/src/accounts/urls.py
@@ -0,0 +1,19 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from accounts.views import UserViewSet
+
+router = DefaultRouter()
+
+router.register("users", UserViewSet, basename='user')
+
+urlpatterns = [
+    # JWT Authentication endpoints
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+] + router.urls
